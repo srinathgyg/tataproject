c6827881a3ada58b9a6e73cf634cb79f877e4721
[srinath gyajangi] <[srinathg363@gmail.com]>
2022-11-04 17:03:33 -0500

    tata local commit

---
+++ TATAPROJECT/src/com/src/tata/Project.java
@@ -0,0 +1,15 @@
+package com.src.tata;
+
+import com.src.tata1.Student;
+
+public class Project {
+
+	public static void main(String[] args) { // start 1
+	
+		Student s1 = new Student();
+		s1.display();
+		s1.display1();
+
+	}
+
+}
---
+++ TATAPROJECT/src/com/src/tata1/Student.java
@@ -0,0 +1,16 @@
+package com.src.tata1;
+
+public class Student {
+
+int i=1;
+int j=2;
+int k = i+j;
+public void display()
+{
+System.out.println(k);
+}
+public void display1()
+ {
+    System.out.println("hello");
+ }
+}
